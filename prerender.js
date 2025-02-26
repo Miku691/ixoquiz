066252bce36d6aaa26ddb12b6f108645b900a6c1
Makaradhwaja Behera (Miku) <57888483+Miku691@users.noreply.github.com>
2025-02-26 12:51:28 +0530

    Update prerender.js

--- prerender.js
+++ prerender.js
@@ -9,7 +9,7 @@ const path = require('path');
   const routes = ['/', '/about', '/contact']; // Add your routes here
 
   for (const route of routes) {
-    await page.goto(`http://localhost:8080${route}`, { waitUntil: 'networkidle0' });
+    await page.goto(`https://miku691.github.io/ixoquiz/${route}`, { waitUntil: 'networkidle0' });
     const content = await page.content();
     const filePath = path.join(__dirname, 'dist', route === '/' ? 'index.html' : `${route}.html`);
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
