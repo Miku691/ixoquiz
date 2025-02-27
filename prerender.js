b35b866c8606f07b2638c3b7564d9cff6009bf26
Makaradhwaja Behera (Miku) <57888483+Miku691@users.noreply.github.com>
2025-02-27 10:38:29 +0530

    Update prerender.js

--- prerender.js
+++ prerender.js
@@ -2,19 +2,24 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
+const routes = [
+  '/',
+  '/about',
+  '/contact',
+  '/quiz/festivals/raja-sankranti-quiz'
+]; // Add your routes here
+
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-
-  const routes = ['/', '/about', '/contact']; // Add your routes here
-
+  
   for (const route of routes) {
-    await page.goto(`https://miku691.github.io/ixoquiz/${route}`, { waitUntil: 'networkidle0' });
+    await page.goto(`https://miku691.github.io/ixoquiz${route}`, { waitUntil: 'networkidle0' });
     const content = await page.content();
     const filePath = path.join(__dirname, 'dist', route === '/' ? 'index.html' : `${route}.html`);
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
     fs.writeFileSync(filePath, content);
   }
-
+  
   await browser.close();
 })();
